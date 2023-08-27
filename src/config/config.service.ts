c8afb1c9ad453359dd41c4fc6ace9251180c8f24
David Liendo <davidals70@gmail.com>
2023-08-27 04:48:40 -0400

    Deploy setup

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,25 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 require('dotenv').config();
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: parseInt('3306'),
-  username: 'root',
-  password: '',
-  database: 'santaines',
-  autoLoadEntities: true,
-  // entities: [],
-  synchronize: true,
-};
 // export const typeOrmConfig: TypeOrmModuleOptions = {
 //   type: 'mysql',
-//   host: process.env.MYSQL_HOST ?? 'localhost',
-//   port: parseInt(process.env.MYSQL_PORT ?? '3306'),
-//   username: process.env.MYSQL_USER ?? 'root',
-//   password: process.env.MYSQL_PASSWORD ?? '',
-//   database: process.env.MYSQL_DB ?? 'santaines',
+//   host: 'localhost',
+//   port: parseInt('3306'),
+//   username: 'root',
+//   password: '',
+//   database: 'santaines',
 //   autoLoadEntities: true,
 //   // entities: [],
 //   synchronize: true,
 // };
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.MYSQL_HOST ?? 'localhost',
+  port: parseInt(process.env.MYSQL_PORT ?? '3306'),
+  username: process.env.MYSQL_USER ?? 'root',
+  password: process.env.MYSQL_PASSWORD ?? '',
+  database: process.env.MYSQL_DB ?? 'santaines',
+  autoLoadEntities: true,
+  // entities: [],
+  synchronize: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  await app.listen(parseInt(process.env.PORT));
+  // await app.listen(8000);
 }
 bootstrap();
