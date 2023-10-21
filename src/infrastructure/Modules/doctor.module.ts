dbabcc115912c0ea15877fa9277de4f4f6d45510
danf1720 <46090978+danf1720@users.noreply.github.com>
2023-10-21 10:41:41 -0400

    Update doctor.module.ts
    arreglo donctor.moduls

--- src/infrastructure/Modules/doctor.module.ts
+++ src/infrastructure/Modules/doctor.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BuscarDoctores } from 'src/aplicacion/doctor/BuscarDocotres';
+import { BuscarDoctorPorCorreo } from 'src/aplicacion/doctor/BuscarDoctorPorCorreo';
+import { BuscarDoctorPorEspecialidad } from 'src/aplicacion/doctor/BuscarDoctorPorEspecialidad';
+import { BuscarDoctorPornombre } from 'src/aplicacion/doctor/BuscarDoctorPorNombre';
+import { ModificarDoctor } from 'src/aplicacion/doctor/ModificarDoctor';
+import { RegistrarDoctorService } from 'src/aplicacion/doctor/RegistrarDoctro';
 import { DoctorEntity } from '../db-entities/doctor.entity';
 import { DoctorRepositoryService } from '../Services/DoctorServices/DoctorRepository.service';
 import { DoctorController } from '../Controllers/Doctor.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DoctorEntity])],
-  controllers: [DoctorController],
-  providers: [DoctorRepositoryService],
-  exports: [DoctorRepositoryService],
+  providers: [BuscarDoctores,
+                BuscarDoctorPorCorreo,
+                BuscarDoctorPorEspecialidad,
+                BuscarDoctorPornombre,
+                ModificarDoctor,
+                RegistrarDoctorService,
+              {provide:'RepositorioDoctor',
+               useClass: DoctorRepositoryService}],
+ controllers: [DoctorController],
+ 
 })
 export class DoctorModule {}
