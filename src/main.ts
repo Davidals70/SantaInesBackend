78469c70d7cfdf8191eec6a4d211dc0521477782
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-21 12:57:24 -0700

    refactor(refactor): :recycle: Add CORS options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import { json as expressJson, urlencoded as expressUrlEncoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.PORT));
-  // await app.listen(8000);
+  const corsOptions: cors.CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
+  app.use(expressJson({ limit: '50mb' }));
+  app.use(expressUrlEncoded({ limit: '50mb', extended: true }));
+
+  // await app.listen(parseInt(process.env.PORT));
+  await app.listen(8000);
 }
 bootstrap();
