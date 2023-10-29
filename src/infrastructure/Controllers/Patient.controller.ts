ba2aa0fb588b9162718dc7ef39a0d69b5b2f6bc7
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-10-28 21:22:35 -0400

    Update Patient.controller.ts
    Se actualiza Patient.controller.ts recibiendo y retornado objetos de tipo DTO para un mejor manejo de los datos.

--- src/infrastructure/Controllers/Patient.controller.ts
+++ src/infrastructure/Controllers/Patient.controller.ts
@@ -1,33 +1,38 @@
-import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { PatientRepositoryService } from '../Services/PatientServices/PatientRepository.service';
 import { PatientEntity } from '../db-entities/patient.entity';
+import { CreatePatientDto } from '../Dtos/CreatePatient.dto';
+import { UpdatePatientDto } from '../Dtos/UpdatePatient.dto';
+import { idDoctor } from 'src/domain/doctor/ValueObject/idDoctor';
 
 @Controller('patient')
 export class PatientController {
   constructor(private readonly patientService: PatientRepositoryService) {}
 
-  @Get()
-  findAll(): Promise<PatientEntity[]> {
+  @Get()//aaa!!!
+  findAll(): Promise<CreatePatientDto[]> {
     return this.patientService.findAll();
   }
 
-  @Get(':id_number')
-  findOneByIdNumber(@Param('id_number') id_number: string): Promise<PatientEntity> {
-    return this.patientService.findOneByIdNumber(id_number);
+  @Get(':id')//aaa!!!
+  findOne(@Param('id') id: string): Promise<CreatePatientDto> {
+    return this.patientService.findOne(id);
   }
 
-  @Post()
-  create(@Body() patientData: Partial<PatientEntity>): Promise<PatientEntity> {
+  @Post()//aaa!!!
+  create(@Body() patientData: CreatePatientDto): Promise<CreatePatientDto> {//Quito el partial porque queremos que sea obligatorio todo
     return this.patientService.create(patientData);
   }
 
-  @Put(':id_number')
-  update(@Param('id_number') id_number: string, @Body() patientData: Partial<PatientEntity>): Promise<PatientEntity> {
-    return this.patientService.update(id_number, patientData);
+  @Put(':id')//aaa!!!
+  update(
+  @Param('id', ParseUUIDPipe) id: string, 
+  @Body() patientData: UpdatePatientDto): Promise<UpdatePatientDto> {//Quito el partial porque no es necesario
+    return this.patientService.update(id, patientData);
   }
 
-  @Delete(':id_number')
-  remove(@Param('id_number') id_number: string): Promise<void> {
-    return this.patientService.remove(id_number);
+  @Delete(':id')//aaa!!!
+  remove(@Param('id') id: string): Promise<void> {
+    return this.patientService.remove(id);
   }
 }
