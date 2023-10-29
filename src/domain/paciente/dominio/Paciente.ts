c595d1bac24cfd4b3adf31dcffaecf80b19804b7
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-10-28 21:19:14 -0400

    Add Getters and Setters in 'Paciente' class
    Se añadio los getters y setters de pacientes,

--- src/domain/paciente/dominio/Paciente.ts
+++ src/domain/paciente/dominio/Paciente.ts
@@ -13,7 +13,7 @@ private genero: string;
 private correo: Correo;
 private id: idPaciente; 
 
- private constructor(nombre :string, apellido: string,fechaNacimiento: FechaNacimiento, cedula: string , Direccion :string,telefono: string, genero: string,  correo: Correo ,id?: idPaciente ){
+constructor(nombre :string, apellido: string,fechaNacimiento: FechaNacimiento, cedula: string , Direccion :string,telefono: string, genero: string,  correo: Correo ,id?: idPaciente ){
     this.nombre=nombre;
     this.apellido=apellido;
     this.fechaNacimiento=fechaNacimiento;
@@ -24,5 +24,80 @@ private id: idPaciente;
     this.correo=correo;
     this.id= id;
 }
+
+
+//getters and setters
+public getNombre(): string {
+    return this.nombre;
+}
+
+public setNombre(nombre: string): void {
+    this.nombre = nombre;
+}
+
+public getApellido(): string {
+    return this.apellido;
+}
+
+public setApellido(apellido: string): void {
+    this.apellido = apellido;
+}
+
+public getFechaNacimiento(): FechaNacimiento {
+    return this.fechaNacimiento;
+}
+
+public setFechaNacimiento(fechaNacimiento: FechaNacimiento): void {
+    this.fechaNacimiento = fechaNacimiento;
+}
+
+public getCedula(): string {
+    return this.cedula;
+}
+
+public setCedula(cedula: string): void {
+    this.cedula = cedula;
+}
+
+public getDireccion(): string {
+    return this.Direccion;
+}
+
+public setDireccion(Direccion: string): void {
+    this.Direccion = Direccion;
+}
+
+public getTelefono(): string {
+    return this.telefono;
+}
+
+public setTelefono(telefono: string): void {
+    this.telefono = telefono;
+}
+
+public getGenero(): string {
+    return this.genero;
+}
+
+public setGenero(genero: string): void {
+    this.genero = genero;
+}
+
+public getCorreo(): Correo {
+    return this.correo;
+}
+
+public setCorreo(correo: Correo): void {
+    this.correo = correo;
+}
+
+public getId(): idPaciente {
+    return this.id;
+}
+
+public setId(id: idPaciente): void {
+    this.id = id;
+}
+
 
 }
