ec8e2c1ad07d0824fd5fff90ba1d7e3cc6783fba
danf1720 <46090978+danf1720@users.noreply.github.com>
2023-10-31 07:53:49 -0400

    Update ModificarDoctorDto.ts

--- src/aplicacion/doctor/DataTransferObject/ModificarDoctorDto.ts
+++ src/aplicacion/doctor/DataTransferObject/ModificarDoctorDto.ts
@@ -2,10 +2,8 @@ export class ModificarDoctorDto{
     IdDoctor: string;
     nombre: string;
     apellido: string;
-    fechaNacimiento: Date;
     especialidad: string;
     cedula: string;
-    Direccion: string;
     telefono: string;
     genero: string;
     correo: string;
