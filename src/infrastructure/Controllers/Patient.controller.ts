49f3b408618416b447680c26bcf2881e33b76489
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-11-11 19:15:48 -0400

    Update Patient.controller.ts
    Actualizando el post del controllador para un mejor el manejo en Arquitectura hexagonal del paciente

--- src/infrastructure/Controllers/Patient.controller.ts
+++ src/infrastructure/Controllers/Patient.controller.ts
@@ -4,10 +4,14 @@ import { PatientEntity } from '../db-entities/patient.entity';
 import { CreatePatientDto } from '../Dtos/CreatePatient.dto';
 import { UpdatePatientDto } from '../Dtos/UpdatePatient.dto';
 import { idDoctor } from 'src/domain/doctor/ValueObject/idDoctor';
+import { PatientFactory } from 'src/domain/Factories/PatientFactory';
+import { Paciente } from 'src/domain/paciente/dominio/Paciente';
+import { isUUID } from 'class-validator';
 
 @Controller('patient')
 export class PatientController {
-  constructor(private readonly patientService: PatientRepositoryService) {}
+  constructor(private readonly patientService: PatientRepositoryService,
+    ) {}
 
   @Get()//aaa!!!
   findAll(): Promise<CreatePatientDto[]> {
@@ -19,15 +23,52 @@ export class PatientController {
     return this.patientService.findOne(id);
   }
 
-  @Post()//aaa!!!
-  create(@Body() patientData: CreatePatientDto): Promise<CreatePatientDto> {//Quito el partial porque queremos que sea obligatorio todo
-    return this.patientService.create(patientData);
+  @Post()//*****
+  async create(@Body() patientData: CreatePatientDto): Promise<Object> {//Quito el partial porque queremos que sea obligatorio todo
+    const patientFactory: PatientFactory = new PatientFactory();
+    const patient: Paciente = patientFactory.CreatePatient(
+      null,
+      patientData.name,
+      patientData.lastname,
+      patientData.birthday,
+      patientData.id_number,
+      patientData.address,
+      patientData.phone_number,
+      patientData.gender,
+      patientData.email,
+    );
+
+    const createdPatient: Paciente = await this.patientService.create(patient);
+    if(createdPatient){
+      const patient: PatientEntity = new PatientEntity();
+      patient.name = createdPatient.getNombre();
+      patient.lastname = createdPatient.getApellido();
+      patient.address = createdPatient.getDireccion();
+      patient.birthday = createdPatient.getFechaNacimiento();
+      patient.id_number = createdPatient.getCedula();
+      patient.phone_number = createdPatient.getTelefono();
+      patient.gender = createdPatient.getGenero();
+      patient.email = createdPatient.getDireccion();
+      patient.ID  = String(createdPatient.getId());
+      return patient;
+    }
+
+
+      return {
+        error:'Error mientras se crea el paciente'
+      };
+    // return this.patientService.create(patientData);
   }
 
   @Put(':id')//aaa!!!
   update(
   @Param('id', ParseUUIDPipe) id: string, 
   @Body() patientData: UpdatePatientDto): Promise<UpdatePatientDto> {//Quito el partial porque no es necesario
+    
+
+
+
+    
     return this.patientService.update(id, patientData);
   }
 
