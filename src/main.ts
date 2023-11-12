0d51553c9c6205304d577c2f2f9dccb39b0f33c2
Angel Hernandez <64968854+Delta27222@users.noreply.github.com>
2023-11-12 08:48:23 -0400

    Update main.ts
    Updating Main.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { json as expressJson, urlencoded as expressUrlEncoded } from 'express';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { json as expressJson, urlencoded as expressUrlEncoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,20 +15,6 @@ async function bootstrap() {
 
   app.use(expressJson({ limit: '50mb' }));
   app.use(expressUrlEncoded({ limit: '50mb', extended: true }));
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    // Opciones adicionales como Access-Control-Allow-Methods, Access-Control-Allow-Headers, etc.
-    next();
-  });
-
-
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist:true,
-  //     forbidNonWhitelisted:true
-  //   }),
-  // )
   
   // await app.listen(parseInt(process.env.PORT));
   await app.listen(8000);
